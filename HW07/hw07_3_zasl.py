46533c16efa07cf5e2bb81e8b87012696d38d47b
fcss88 <fcss@ukr.net>
2023-06-19 00:31:20 +0300

    add hw07

--- HW07/hw07_3_zasl.py
+++ HW07/hw07_3_zasl.py
@@ -9,13 +9,27 @@ def average(lst):
 
 def median(lst):
     sorted_lst = sorted(lst)
+    print("Сортований список:", end=" ")
     n = len(sorted_lst)
     if n % 2 == 0:
         mid1 = sorted_lst[n // 2 - 1]
         mid2 = sorted_lst[n // 2]
+        for i in sorted_lst:
+            if i == mid1 or i == mid2:
+                print("\033[91m" + str(i) + "\033[0m", end=" ")
+            else:
+                print(i, end=" ")
+        print("\n")
         return (mid1 + mid2) / 2
     else:
-        return sorted_lst[n // 2]
+        mid = sorted_lst[n // 2]
+        for i in sorted_lst:
+            if i == mid:
+                print("\033[91m" + str(i) + "\033[0m", end=" ")
+            else:
+                print(i, end=" ")
+        print("\n")
+        return mid
 
 
 numbers = [
@@ -29,7 +43,14 @@ numbers = [
     random.randint(1, 100),
     random.randint(1, 100),
     random.randint(1, 100),
-    random.randint(1, 100),
+]
+print("Наш масив чисел, парна кількість: ", numbers)
+avg = average(numbers)
+med = median(numbers)
+print("Середнє значення:", avg)
+print("Медіана:", med)
+
+numbers2 = [
     random.randint(1, 100),
     random.randint(1, 100),
     random.randint(1, 100),
@@ -40,9 +61,8 @@ numbers = [
     random.randint(1, 100),
     random.randint(1, 100),
 ]
-##[5, 2, 7, 10, 3]
-print(numbers)
-avg = average(numbers)
-med = median(numbers)
+print("Наш масив чисел, непарна кількість: ", numbers2)
+avg = average(numbers2)
+med = median(numbers2)
 print("Середнє значення:", avg)
 print("Медіана:", med)
